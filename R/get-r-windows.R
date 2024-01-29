9745183475f75c631cf1a14049eceddb659b146a
changwoolim <limcw@zarathu.com>
2024-01-29 10:03:59 +0900

    Testing: Download innoextract when user starts packing

--- R/get-r-windows.R
+++ R/get-r-windows.R
@@ -1,18 +1,28 @@
 get_r_windows <- function() { # Define package name and paths
   your_package_name <- "executablePackeR"
-  innoextract_path <- system.file("innoextract/innoextract.exe", package = your_package_name)
+  # innoextract_path <- system.file("innoextract/innoextract.exe", package = your_package_name)
   R_version <- as.character(getRversion())
   r_installer_url <- paste0("https://cloud.r-project.org/bin/windows/base/old/", R_version, "/R-", R_version, "-win.exe")
   r_dir <- "r-win"
-
+  innoextract_url <- "https://constexpr.org/innoextract/files/innoextract-1.9-windows.zip"
+  innoextract_url_mirror <- "https://github.com/dscharrer/innoextract/releases/download/1.9/innoextract-1.9-windows.zip"
+  innoextract_filename <- "innoextract.zip"
+  innoextract_path <- "innoextract"
   # Create download directory
   dir.create(r_dir)
 
   # Download R installer
+  print("Downloading R Installer from r-project.org")
   download.file(r_installer_url, file.path(r_dir, "r_windows.exe"), mode = "wb")
+  print("Downloading R Installer Complete")
+
+  download_file(primary_url = innoextract_url,
+                mirror_urls = c(innoextract_url_mirror),
+                dest_file = "innoextract.zip", name = "innoextract")
+  unzip(innoextract_filename, exdir = innoextract_path)
 
   # Construct command to extract the R installer
-  cmd_extract <- paste(shQuote(innoextract_path), "-e", shQuote(file.path(r_dir, "r_windows.exe")))
+  cmd_extract <- paste(shQuote(paste(innoextract_path, innoextract_filename, sep="/")), "-e", shQuote(file.path(r_dir, "r_windows.exe")))
 
   # Execute the extraction command
   system(cmd_extract, invisible = TRUE)
