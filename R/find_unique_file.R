00ca6dc2ca0d9aa08d742d4dcc2bf81a9f91b41a
changwoolim <limcw@zarathu.com>
2024-02-13 18:00:50 +0900

    Create find_unique_file.R

---
+++ R/find_unique_file.R
@@ -0,0 +1,19 @@
+find_unique_file <- function(start_dir) {
+  current_dir <- start_dir
+
+  # Loop until there are no more subdirectories
+  while(TRUE) {
+    subdirs <- list.dirs(current_dir, full.names = TRUE, recursive = FALSE)
+    if(length(subdirs) == 0) break # Exit loop if no more subdirectories
+    current_dir <- subdirs[1] # Move to the next subdirectory
+  }
+
+  # Now, list files in the final directory
+  files <- list.files(current_dir, full.names = TRUE)
+
+  if(length(files) == 1) {
+    return(files[1]) # Return the full path of the unique file
+  } else {
+    stop("No files or multiple files found in the final directory.")
+  }
+}
